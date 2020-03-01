20afa92460a5ee1c0c3210790f334fa9f07e12a2
alexandre <acuenca@quotemedia.com>
2020-02-29 21:42:59 -0800

    fixed a rounding issue

---
+++ src/test/java/me/cuenca/price/port/adapter/rest/PricesPayloadTest.java
@@ -0,0 +1,27 @@
+package me.cuenca.price.port.adapter.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import me.cuenca.price.PriceModule;
+import me.cuenca.price.domain.model.eod.Quote;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PricesPayloadTest {
+
+  @Autowired
+  private ObjectMapper mapper = new PriceModule().mapper();
+
+  @Test
+  void shouldDeserializeCents() throws Exception {
+    String json = "{\"symbol\":\"FR123456\",\"prices\":[{\"timepoint\":\"1995-01-02\",\"quote\":{\"open\":307.27,\"high\":305.28,\"low\":305.27,\"close\":304.28,\"volume\":10000}}]}";
+
+    PricesPayload prices = mapper.readValue(json, PricesPayload.class);
+
+    Quote quote = prices.prices.get(0).getQuote();
+    assertEquals(ImmutableList.of(30727,30528,30527,30428,10000), quote.toDocument());
+  }
+
+}
